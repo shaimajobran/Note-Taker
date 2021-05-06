4e93de3f3409cdb6354a2cad323f0f86c11381f5
shaimaJobran <shaimajobran222gmail.com>
2021-05-06 17:01:03 -0400

    another try

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+//dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 //creating server using express()
-const express = require('express'),
-      app = express();
-      PORT = process.env.PORT || 1000;
-      apiRoute = require("./routes/route-API");
-      htmlRoute = require("./routes/route-HTML");
-
+const app = express();
+const PORT = process.env.PORT ||1000 ;
 //builtin middleware for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,11 +15,50 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //importing routes.js file 
-apiRoute(app);
-htmlRoute(app);
+app.get("/", function (req ,res) {
+   res.sendFile(path.join(__dirname, 'public/index.html'));
+});  
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, 'public/notes.html')); 
+});
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, '/db/db.json'));  
+});
 
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", function(error, response) {
+        if (error) {
+            console.log(error);
+        }
+        const notes = JSON.parse(response);
+        const noteRequest = req.body;
+        const newNoteId = notes.length + 1;
+        const newNote = {
+            id: newNoteId,
+            title: noteRequest.title,
+            text: noteRequest.text,
+        };
+        notes.push(newNote);
+        res.json(newNote);
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(notes, null, 2), function(err) {
+            if (err) throw err;
+        });
+    });
+});
+
+// //delete Data 
+// app.delete('/api/notes/:id', (request, response) => {
+
+//     const selectedNoteID = request.params.id;
+//     console.log(`Delete item with id: ${selectedNoteID}`);
+
+//     // //remove item from notes array
+//     notes = notes.filter(note => note.id != selectedNoteID);
+
+//     response.end();
+// });
 
 //start server to listen 
 app.listen(PORT, () => {
-    console.log('app is running at http://localhost:' + PORT);
+    console.log(`app is listening on ${PORT}.`);
 });
